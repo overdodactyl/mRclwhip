89c83e667d1e5dde467978434e88e1ab4dce8710
overdodactyl <patjohns1216@gmail.com>
2020-02-17 11:31:20 -0500

    scales package dependency

--- R/nrange.R
+++ R/nrange.R
@@ -8,7 +8,7 @@
 #' @param name Name of new column.
 #' @param remove If TRUE, remove input columns from output data frame.
 #' @param formatter scales function used to format text
-#' @importFrom scales percent number comma
+
 #' @return An object of the same class as .data.
 #' @examples
 #' df <- tibble::tribble(
@@ -21,22 +21,25 @@
 #' @export
 
 nrange <- function(.data, n, low, high, accuracy = 0.01, name = "Estimate",
-                   formatter = c("comma", "percent", "number") , remove = TRUE, ...) {
+                   formatter = c("comma", "percent", "number") , remove = TRUE) {
+
+  if (!"scales" %in% loadedNamespaces())
+    attachNamespace("scales")
 
   formatter <- match.arg(formatter)
   format_func <- match.fun(formatter)
 
-  n <- enquo(n)
-  low <- enquo(low)
-  high <- enquo(high)
-
   res <- .data %>%
-    mutate_at(vars(!!n, !!low, !!high), list(~ format_func(., accuracy, ...))) %>%
-    mutate(!!name := paste0(!!n, " (", !!low, ", ", !!high, ")"))
+    mutate_at(vars({{n}}, {{low}}, {{high}}), list(~ format_func(., accuracy = accuracy))) %>%
+    mutate({{name}} := paste0({{n}}, " (", {{low}}, ", ", {{high}}, ")"))
 
   if (remove) {
-    res <- select(res, -!!n, -!!low, -!!high)
+    res <- select(res, -{{n}}, -{{low}}, -{{high}})
   }
 
   res
 }
+
+##' @importFrom scales comma
+##' @export
+NULL
--- tests/testthat/test-measure-summary.R
+++ tests/testthat/test-measure-summary.R
@@ -1,15 +1,15 @@
 test_that("n range", {
-  df <- tibble::tribble(
+  df1 <- tibble::tribble(
     ~Group, ~n, ~low, ~high,
     "a", 1.323453, 1.154, 1.44,
     "b", 1000.801, 987.741, 1010.9891
   )
 
-  expect_identical(nrange(df, n, low, high, accuracy = .1)$Estimate[2], "1,000.8 (987.7, 1,011.0)")
-  expect_identical(nrange(df, n, low, high, accuracy = .1) %>% names(), c("Group", "Estimate"))
-  expect_identical(nrange(df, n, low, high, name = "Result") %>% names(), c("Group", "Result"))
+  expect_identical(nrange(df1, n, low, high, formatter = "comma", accuracy = .1)$Estimate[2], "1,000.8 (987.7, 1,011.0)")
+  expect_identical(nrange(df1, n, low, high, accuracy = .1) %>% names(), c("Group", "Estimate"))
+  expect_identical(nrange(df1, n, low, high, name = "Result") %>% names(), c("Group", "Result"))
   expect_identical(
-    nrange(df, n, low, high, remove = F) %>% names(),
+    nrange(df1, n, low, high, remove = F) %>% names(),
     c("Group", "n", "low", "high", "Estimate")
   )
 })
